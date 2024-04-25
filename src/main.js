d2cbf5672e8e24c97a859c1e98431610da58392a
Valentin renaudin <valentin.renaudin@my-digital-school.org>
2024-04-25 19:13:46 +0200

    [FIX] Merge old commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const titre = document.getElementById('titre');
 const contenu = document.getElementById('contenu');
 const addBox = document.querySelector('.add-box');
@@ -44,45 +43,4 @@ function toggleModal() {
 function clearToggle() {
     titleTag.value = '';
     descTag.value = '';
-    
-};
-=======
-const { invoke } = window.__TAURI__.tauri;
-
-function creerNote() {
-  const titre = document.getElementById("titre").value;
-  const contenu = document.getElementById("contenu").value;
-
-  const note = document.createElement("div");
-  note.classList.add("bg-white", "p-4", "rounded-lg", "note"); // J'ai ajouté 'note' à la classe pour que la fonction supprimerNote fonctionne correctement
-  note.innerHTML = `
-      <h1 class="font-bold text-lg">${titre}</h1>
-      <p>${contenu}</p>
-      <button onclick="supprimerNote(this)">Supprimer</button>
-  `;
-  document.getElementById("savedNotes").appendChild(note);
-  insererFichierLocal(titre, contenu); // Passez les valeurs directement à la fonction suivante
 }
-
-async function insererFichierLocal(titre, contenu) {
-  const note = {
-    titre: titre,
-    contenu: contenu,
-  };
-
-  console.log("Debug note object:", note); // Pour vérifier l'objet
-
-  try {
-    const fichier = await invoke("save_note", { note }); // Assurez-vous que l'objet est bien passé en tant que 'note'
-    console.log(fichier);
-  } catch (error) {
-    console.error("Erreur lors de l'invocation de save_note:", error);
-  }
-}
-
-
-
-function supprimerNote(button) {
-  button.parentNode.remove(); // Cela supprime la div de la note entière
-}
->>>>>>> 9cd9202980936b75a62bef005c6bb8dd2d74a690
