c16f41344cb6b69240753dd1bda5140d2142b25d
Valentin renaudin <valentin.renaudin@my-digital-school.org>
2024-04-25 22:39:34 +0200

    [ADD] Rust link Front [ADD] JSON to Front [ADD] Delete note in JSON

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,46 +1,194 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-use serde::{Deserialize, Serialize};
-use std::fs::File;
-use std::io::Write;
-
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-
-#[tauri::command]
-fn get_message() -> String {
- "Hello from Rust!".to_string()
-}
+use std::io::Error;
+use std::fs;
+use chrono::{DateTime, Utc};
+use serde::{Serialize, Deserialize}; // Importation des traits Serialize et Deserialize
+// use rusqlite::{params, Connection, Result};
 
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 struct Note {
-    titre: String,
-    contenu: String,
+  id: i32,
+  title: String,
+  description: String,
+  date: String,
+}
+
+// Fonction pour créer une nouvelle note et la stocker dans un fichier JSON
+#[tauri::command]
+fn create_note(title: String, description: String) -> Result<Vec<Note>, String> {
+  // Créer la nouvelle note
+  let id = generate_unique_id();
+  let date = Utc::now();
+  let date_str = date.format("%d/%m/%Y").to_string();
+
+  let new_note = Note {
+    id,
+    title,
+    description,
+    date: date_str,
+  };
+
+  // Lire les notes existantes depuis le fichier JSON
+  let mut notes = read_notes().unwrap_or_else(|err| {
+    eprintln!("Erreur lors de la lecture des notes: {}", err);
+    Vec::new() // Si une erreur se produit lors de la lecture des notes, créer une nouvelle liste vide
+  });
+
+  // Ajouter la nouvelle note à la liste des notes
+  notes.push(new_note.clone());
+
+  // Écrire la liste mise à jour dans le fichier JSON
+  if let Err(err) = write_notes(&notes) {
+    eprintln!("Erreur lors de l'écriture des notes: {}", err);
+    return Err("Erreur lors de l'écriture des notes".to_string());
+  }
+
+  // Lire le fichier à nouveau et renvoyer les données
+  match read_notes() {
+    Ok(updated_notes) => {
+      println!("Notes mises à jour : {:?}", updated_notes);
+      Ok(updated_notes)
+    }
+    Err(err) => {
+      eprintln!("Erreur lors de la lecture des notes mises à jour: {}", err);
+      Err("Erreur lors de la lecture des notes mises à jour".to_string())
+    }
+  }
+}
+
+fn generate_unique_id() -> i32 {
+  // Lire les notes depuis le fichier JSON
+  let notes_result = read_notes();
+
+  // Vérifier s'il y a une erreur lors de la lecture des notes
+  if let Err(err) = notes_result {
+    eprintln!("Erreur lors de la récupération des notes: {}", err);
+    return -1; // Retourner une valeur d'erreur
+  }
+
+  let notes: Vec<Note> = notes_result.unwrap_or_else(|_| vec![]);
+
+
+  // Récupérer l'ID de la dernière note
+  let last_note_id = notes.last().map_or(0, |note| note.id);
+
+  // Générer un nouvel ID unique en incrémentant l'ID de la dernière note
+  let new_id = last_note_id + 1;
+
+  new_id
 }
 
 #[tauri::command]
-fn save_note(note: Note) {
-    let mut file = File::create("notes.json").expect("La création du fichier a échoué");
-    let note_json = serde_json::to_string(&note).expect("La sérialisation JSON a échoué");
-    file.write_all(note_json.as_bytes()).expect("L'écriture dans le fichier a échoué");
+fn fetch_notes() -> Result<Vec<Note>, String> {
+  // Lire les notes depuis le fichier JSON
+  let notes = read_notes()?;
+  if !notes.is_empty() {
+    Ok(notes)
+  }
+  else{
+    eprintln!("Erreur lors de la lecture des notes mises à jour");
+    Err("Erreur lors de la lecture des notes mises à jour".to_string())
+  }
+
 }
 
-//Lire les notes depuis le fichier json
-fn read_notes() -> Vec<Note> {
-    let file = File::open("notes.json").expect("L'ouverture du fichier a échoué");
-    let notes: Vec<Note> = serde_json::from_reader(file).expect("La désérialisation JSON a échoué");
-    notes
+
+// Fonction pour lire les notes depuis le fichier JSON
+fn read_notes() -> Result<Vec<Note>, String> {
+  let file_contents = fs::read_to_string("notes.json")
+      .map_err(|err| err.to_string())?; // Convertir l'erreur en String
+
+  let notes: Vec<Note> = serde_json::from_str(&file_contents)
+      .map_err(|err| err.to_string())?; // Convertir l'erreur en String
+
+  Ok(notes)
 }
- 
+
+// Fonction pour écrire les notes dans le fichier JSON
+fn write_notes(notes: &[Note]) -> Result<(), Error> {
+  let notes_json = serde_json::to_string(notes)?;
+  match fs::write("notes.json", notes_json) {
+    Ok(_) => (),
+    Err(e) => eprintln!("Erreur lors de l'écriture des notes : {}", e),
+  }
+  Ok(())
+}
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn update_note(id: String, newTitle: String, new_description: String) {
+  // Convertir l'ID de String en i32
+  let id_i32: Result<i32, _> = id.parse();
+
+  // Vérifier si la conversion a réussi
+  match id_i32 {
+    Ok(id_i32) => {
+      // Maintenant id_i32 est de type i32, vous pouvez l'utiliser
+      // Appeler la fonction pour lire les notes
+      let notes_result = read_notes();
+
+      // Vérifier si la lecture des notes a réussi
+      if let Ok(mut notes) = notes_result {
+        // Rechercher la note correspondant à l'ID donné
+        if let Some(note) = notes.iter_mut().find(|note| note.id == id_i32) {
+          // Mettre à jour le titre et la description de la note
+          note.title = newTitle;
+          note.description = new_description;
+          // Écrire les notes mises à jour dans le fichier
+          if let Err(err) = write_notes(&notes) {
+            println!("Failed to write updated notes: {}", err);
+          }
+        } else {
+          println!("Note with ID {} not found", id_i32);
+        }
+      } else {
+        println!("Error reading notes");
+      }
+    }
+    Err(_) => {
+      println!("Failed to parse ID as i32");
+    }
+  }
 }
+
+#[tauri::command]
+fn delete_note(id: String) {
+  // Convertir l'ID de String en i32
+  let id_i32: Result<i32, _> = id.parse();
+
+  // Vérifier si la conversion a réussi
+  match id_i32 {
+    Ok(id_i32) => {
+      // Maintenant id_i32 est de type i32, vous pouvez l'utiliser
+      // Appeler la fonction pour lire les notes
+      let notes_result = read_notes();
+
+      // Vérifier si la lecture des notes a réussi
+      if let Ok(mut notes) = notes_result {
+        // Trouver l'index de la note avec l'ID donné
+        if let Some(index) = notes.iter().position(|note| note.id == id_i32) {
+          // Supprimer la note de la liste
+          notes.remove(index);
+          // Écrire les notes mises à jour dans le fichier
+          if let Err(err) = write_notes(&notes) {
+            println!("Failed to write updated notes: {}", err);
+          }
+        } else {
+          println!("Note with ID {} not found", id_i32);
+        }
+      } else {
+        println!("Error reading notes");
+      }
+    }
+    Err(_) => {
+      println!("Failed to parse ID as i32");
+    }
+  }
+}
+
 
 fn main() {
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet, save_note])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+  tauri::Builder::default()
+      .invoke_handler(tauri::generate_handler![create_note, fetch_notes,update_note,delete_note])
+      .run(tauri::generate_context!())
+      .expect("erreur lors de l'exécution de l'application Tauri");
 }
-
--- src/main.js
+++ src/main.js
@@ -8,44 +8,46 @@ const addButton = document.querySelector('#addNote');
 const id = document.getElementById("id"); // Déclarer 'id' avant de l'utiliser
 let noteCount = 0;
 // Fonction pour créer une note
-function creerNote() {
-  noteCount++; // Incrémenter le compteur de notes
+async function creerNote() {
+  const { invoke } = window.__TAURI__.tauri;
   const noteId = id.value;
-  if (noteId) {
-      updateNote(noteId);
-  } else {
-      // Créer une div pour la note avec un ID unique basé sur le compteur de notes
-      const note = document.createElement('div');
-      note.id = `note-${noteCount}`;
-      note.classList.add('note', 'flex', 'items-center', 'justify-center', 'justify-between', 'border', 'border-gray-300', 'bg-slate-900', 'rounded-lg', 'p-4', 'm-4');
-      note.innerHTML = `
-          <div class="left-content flex flex-col justify-between py-2">
-              <h1 class="text-4xl">${titre.value}</h1>
-              <p class="text-slate-400">${contenu.value}</p>
-          </div>
-          <div class="right-content flex flex-col justify-center items-center gap-2">
-              <button class="hover:scale-125 duration-200" onclick="editNote('${note.id}', '${titre.value}', '${contenu.value}')">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                      <path fill="currentColor" d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75zM20.71 7.04a.996.996 0 0 0 0-1.41l-2.34-2.34a.996.996 0 0 0-1.41 0l-1.83 1.83l3.75 3.75z"/>
-                  </svg>
-              </button>
-              <button class="hover:scale-125 duration-200" onclick="supprimerNote('${note.id}')">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                      <path fill="currentColor" d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6zm2.46-7.12l1.41-1.41L12 12.59l2.12-2.12l1.41 1.41L13.41 14l2.12 2.12l-1.41 1.41L12 15.41l-2.12 2.12l-1.41-1.41L10.59 14zM15.5 4l-1-1h-5l-1 1H5v2h14V4z"/>
-                  </svg>
-              </button>
-          </div>
-      `;
-      document.body.appendChild(note);
+
+  try {
+    if (noteId) {
+      // Si l'ID de la note existe, mettre à jour la note existante
+      await updateNote();
+      await invoke('update_note', { id: noteId, newTitle: titre.value, newDescription: contenu.value });
+    } else {
+      // Sinon, créer une nouvelle note
+      const newNote = { title: titre.value, description: contenu.value };
+      const updatedNotes = await invoke('create_note', newNote);
+      // Traiter les données retournées si nécessaire
+    }
+
+    // Réinitialiser les champs après l'ajout ou la mise à jour de la note
+    titre.value = '';
+    contenu.value = '';
+    toggleModal();
+  } catch (error) {
+    console.error("Erreur lors de la création ou de la mise à jour de la note:", error);
   }
-  id.value = ''; // Réinitialiser noteId après avoir créé une nouvelle note
 }
 
 
-// Fonction pour supprimer une note
-function supprimerNote(noteId) {
-  const note = document.getElementById(noteId);
-  note.parentNode.removeChild(note);
+async function supprimerNote(noteId) {
+  const { invoke } = window.__TAURI__.tauri;
+
+  try {
+    // Supprimer la note avec l'ID spécifié
+    await invoke('delete_note', { id: noteId });
+    // Supprimer la note du DOM
+    const noteElement = document.getElementById(noteId);
+    if (noteElement) {
+      noteElement.remove();
+    }
+  } catch (error) {
+    console.error("Erreur lors de la suppression de la note:", error);
+  }
 }
 
 // Fonction pour éditer une note
@@ -71,15 +73,57 @@ function updateNote() {
   toggleModal(); 
 }
 
-
-
-
+// Fonction pour afficher ou masquer la fenêtre modale
 function toggleModal() {
     document.getElementById('modal').classList.toggle('hidden');
     clearToggle();
 }
 
+// Fonction pour nettoyer la modal
 function clearToggle() {
     titleTag.value = '';
     descTag.value = '';
 }
+
+// Fonction pour intégrer le fichier json au front-end
+async function initialiseNotes() {
+  try {
+    const { invoke } = window.__TAURI__.tauri;
+    // Appeler la commande back-end pour récupérer les notes
+    const notes = await invoke("fetch_notes");
+    
+    // Traiter les notes récupérées
+    notes.forEach((note) => {
+      const noteElement = document.createElement('div');
+      noteElement.id = `note-${note.id}`;
+      noteElement.classList.add('note', 'flex', 'items-center', 'justify-center', 'justify-between', 'border', 'border-gray-300', 'bg-slate-900', 'rounded-lg', 'p-4', 'm-4');
+      noteElement.innerHTML = `
+        <div class="left-content flex flex-col justify-between py-2">
+          <h1 class="text-4xl">${note.title}</h1>
+          <p class="text-slate-400">${note.description}</p>
+        </div>
+        <div class="right-content flex flex-col justify-center items-center gap-2">
+          <button class="hover:scale-125 duration-200" onclick="editNote('${note.id}')">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+              <path fill="currentColor" d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75zM20.71 7.04a.996.996 0 0 0 0-1.41l-2.34-2.34a.996.996 0 0 0-1.41 0l-1.83 1.83l3.75 3.75z"/>
+            </svg>
+          </button>
+          <button class="hover:scale-125 duration-200" onclick="supprimerNote('${note.id}')">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+              <path fill="currentColor" d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6zm2.46-7.12l1.41-1.41L12 12.59l2.12-2.12l1.41 1.41L13.41 14l2.12 2.12l-1.41 1.41L12 15.41l-2.12 2.12l-1.41-1.41L10.59 14zM15.5 4l-1-1h-5l-1 1H5v2h14V4z"/>
+            </svg>
+          </button>
+        </div>
+      `;
+      document.body.appendChild(noteElement);
+    });
+  } catch (error) {
+    console.error("Erreur lors de l'invocation de fetch_notes:", error);
+  }
+}
+
+
+// Encapsulation dans une fonction asynchrone auto-invoquée
+(async function init() {
+  await initialiseNotes();
+})();
